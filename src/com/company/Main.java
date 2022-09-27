5fb62e75b0360994bd71b46073a20b2afb81c6ca
IsmailovArs <arakb90@gmail.com>
2022-09-27 17:46:41 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int index = 15;
+        for (int i = 0; i < index; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
+
+
+
